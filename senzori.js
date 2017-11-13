aa8f127989dc5c737b4b2c551c8cffc56fa8edb9
AndreiVesa <33317506+AndreiVesa@users.noreply.github.com>
2017-11-13 19:17:41 +0200

    Add files via upload

---
+++ senzori.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_orientation(e)
+{
+	
+	document.getElementById("id_x").innerHTML = e.beta;
+	document.getElementById("id_y").innerHTML = e.gamma;
+	document.getElementById("id_z").innerHTML = e.alpha;
+	
+}
