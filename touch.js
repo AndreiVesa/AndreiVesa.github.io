863b4e9bbc83948844ed3144497ae4f24944b531
AndreiVesa <33317506+AndreiVesa@users.noreply.github.com>
2017-12-14 18:08:30 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,9 +1,11 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.1";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
 canvans.addEventListener("touchstart", on_start_touch);
+
+//var lista_culori = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF" "#FFFFFF")
 
 //-----------------------
 function on_start_touch(e)
@@ -13,7 +15,8 @@ function on_start_touch(e)
 	{
 		context.beginPath();
 		context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2 * Math.PI);
-		context.stroke();
+		context.strokeStyle = 
+		context.stroke() = lista_culori[i];
 	}
 }
 //-----------------------
