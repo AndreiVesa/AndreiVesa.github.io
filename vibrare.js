cf8b7b8b91317f14b20df3d4ef4fb81f6fa1e32c
AndreiVesa <33317506+AndreiVesa@users.noreply.github.com>
2018-01-28 15:29:37 +0200

    Delete vibrare.js

--- vibrare.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("id_business_version").innerHTML="Business version = 2018.01.25.0";
-
-webkitwindow.navigator.vibrate(200); // vibrate for 200ms
-webkitwindow.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
