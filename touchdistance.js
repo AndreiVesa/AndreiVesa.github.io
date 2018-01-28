ecf18c66e6237b8786339b5cde2da5d54b5b3ea8
AndreiVesa <33317506+AndreiVesa@users.noreply.github.com>
2018-01-28 20:20:57 +0200

    Update touchdistance.js

--- touchdistance.js
+++ touchdistance.js
@@ -5,17 +5,10 @@ window.addEventListener('load', function(){
     var startx = 0
     var dist = 0
  
-    box1.addEventListener('touchstart', function(e){
-        var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
-        startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
-        statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px'
-        e.preventDefault()
-    }, false)
- 
     box1.addEventListener('touchmove', function(e){
         var touchobj = e.changedTouches[0] // reference first touch point for this event
         var dist = parseInt(touchobj.clientX) - startx
-        statusdiv.innerHTML = 'Status: touchmove<br> Distance traveled: ' + dist + 'px'
+        statusdiv.innerHTML = 'Status: touchmove<br> Distanta de la pct B la B: ' + dist + 'px'
         e.preventDefault()
     }, false)
  
