c73874e151fc0baaffcba2ab7d42628c12663fac
AndreiVesa <33317506+AndreiVesa@users.noreply.github.com>
2019-01-18 21:14:22 +0200

    Update PDM.js

--- PDM.js
+++ PDM.js
@@ -1,22 +1,3 @@
-window.addEventListener("deviceorientation",on_device_orientation);
-window.addEventListener("deviceorientation",on_device_motion);
-
-function on_device_orientation(e)
-{
-	
-	document.getElementById("id_x").innerHTML = "x=" +e.beta;
-	document.getElementById("id_y").innerHTML = "y="+e.gamma;
-	document.getElementById("id_z").innerHTML = "z="+e.alpha;
-	
-}
-
-//----------------
-
-function on_device_motion(e)
-{
-}
-//----------------
-
 window.addEventListener('load', function(){
  
     var box1 = document.getElementById('box1')
